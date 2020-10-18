bec62c077cabf95d4139f6adc5799c1e5b111f20
Erico da Silva Bruner <erico.bruner@redeunifique.com.br>
2020-10-18 13:28:06 -0300

    atualizando ...

--- src/index.py
+++ src/index.py
@@ -1,7 +1,7 @@
 import urllib.request
 url = input("Enter the Youtube-url\n")
 name = input("Enter the name for the video\n")
-name=name+".mp4"
+name = name+".mp4"
 try:
     print("Downloading starts...\n")
     urllib.request.urlretrieve(url, name)
